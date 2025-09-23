b9a81fd425bc8223eebdc829e151ce2b1db97f4f
Alexey Shlyk <danteal65@gmail.com>
2025-09-23 18:37:14 +0300

    Nudging

---
+++ Assets/MainProject/Scripts/Nudger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Nudger : MonoBehaviour
+{
+    [Header("Nudge on land")]
+    public float minNudgeForce = 0.5f;
+    public float maxNudgeForce = 2.0f;
+
+
+    private Rigidbody rb;
+
+    private bool landed = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!landed && collision.gameObject.CompareTag("Ground"))
+        {
+            landed = true;
+
+            Vector2 dir2d = Random.insideUnitCircle;
+            if (dir2d.sqrMagnitude < 0.001f) dir2d = new Vector2(1f, 0f);
+            dir2d.Normalize();
+            Vector3 nudge = new Vector3(dir2d.x, 0f, dir2d.y);
+            float force = Random.Range(minNudgeForce, maxNudgeForce);
+            rb.AddForce(nudge * force, ForceMode.Impulse);
+        }
+    }
+
+}
